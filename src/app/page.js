e2a00f15628cb1902ecca0e54fc5466984ed17db
minseochh02 <minseochh02@gmail.com>
2025-01-17 12:09:35 +0900

    fixed images

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,5 @@
 import LevelTestPoster from "../components/LevelTestPoster";
 
 export default function Home() {
-	return (
-		<main className="min-h-screen p-4">
-			<LevelTestPoster />
-		</main>
-	);
+	return <main className="min-h-screen p-4"></main>;
 }
